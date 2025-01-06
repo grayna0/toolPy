32e323c2fa2c15068002d0036323993a2f00a398
G-ray <grayna0164@gmail.com>
2025-01-06 20:28:01 +0700

    6/1/25-fixing translate texxt (Chatgpt) - pendding

--- functions/Get_Sub_bilibili.py
+++ functions/Get_Sub_bilibili.py
@@ -19,10 +19,11 @@ def getSub(api_url,video_id):
         ojbSub = video_info['body']
     listSub= "" 
     for contents in ojbSub:
-            listSub = f"{listSub} /n {contents['content']}"
-    with open("C:/Users/pc/myPyPro/result/listSub.txt", 'a+',encoding='utf-8') as file:
+            listSub = f"{listSub} \n {contents['content']}"
+    with open("C:/Users/pc/toolPy/result/listSub.txt", 'a',encoding='utf-8') as file:
          file.write(listSub)
     return listSub  
+getSub(api_url="https://aisubtitle.hdslb.com/bfs/ai_subtitle/prod/1136056766995172720989332742d457eb452b83b475056ae2974d8c79?auth_key=1736150676-88a6bf701d354c848d0178651013d2cb-0-485566df72f204cb9fb0934ab51cedba",video_id="https://www.bilibili.com/video/BV1xMiBYUEBM/?spm_id_from=333.337.search-card.all.click&vd_source=1124f7b904b73e398c740b637205feb4")
          
 
          
